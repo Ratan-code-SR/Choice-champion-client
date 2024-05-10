1b4217260130eae5472396dcce4b18b6caef579a
ratan <ratanchakma68@gmail.com>
2024-05-10 08:27:46 +0600

    firebase functionality added

--- src/components/provider/ContextProvider.jsx
+++ src/components/provider/ContextProvider.jsx
@@ -1,12 +1,63 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import PropTypes from 'prop-types'; // ES6
+import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import auth from '../../firebase/firebase.config'
 
 export const AuthContext = createContext()
 const ContextProvider = ({ children }) => {
     const [loading, setLoading] = useState(true)
-    const info = {loading}
+    const [user, setUser] = useState(null)
+
+
+    // create user with email and password
+    const signUpUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    // log in user with email and password
+    const signInUser = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    // log in user with google
+    const signInWithGoogle = () => {
+        setLoading(true)
+        const provider = new GoogleAuthProvider();
+        return signInWithPopup(auth, provider)
+    }
+
+
+    // sign out user 
+    const logOutUser = () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+
+// user profile updated
+const updateUserProfile = (name,photo)=>{
+    setLoading(true)
+    return updateUserProfile(auth.currentUser, {
+        displayName: name, photoURL: photo
+      })
+}
+
+    // user authentication state observer
+    useEffect(() => {
+        const unsubscribed = onAuthStateChanged(auth, (currentUser) => {
+            setLoading(false)
+            setUser(currentUser)
+        })
+        return () => unsubscribed()
+    },[])
+    console.log(user);
+
+
+
+    const authInfoData = { loading, signUpUser, signInUser, signInWithGoogle, logOutUser,user,setUser,updateUserProfile }
     return (
-        <AuthContext.Provider value={info}>
+        <AuthContext.Provider value={authInfoData}>
             {children}
         </AuthContext.Provider>
     );
